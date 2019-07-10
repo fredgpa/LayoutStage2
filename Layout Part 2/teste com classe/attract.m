df4f14e9905a967c74025734bc9993ff6cc9bed6
fredgpa <fred_gpa@hotmail.com>
2019-07-10 12:34:35 -0300

    0.9.9.5

--- Layout Part 2/teste com classe/attract.m
+++ Layout Part 2/teste com classe/attract.m
@@ -1,4 +1,4 @@
-function [ mod ] = attract(dept_number, departments, constraints)
+function [ departments, mod ] = attract(dept_number, departments, constraints)
     
     mod = [0 0];
     attracted_dept = [];
@@ -11,13 +11,25 @@ function [ mod ] = attract(dept_number, departments, constraints)
                 dept2 = findDepartment(departments, constraints(i).deptA);
             end
 
-            if departments(dept1).centroidX > departments(dept2).centroidX              
+            if departments(dept1).centroidX > departments(dept2).centroidX
+                %{
+                departments = adjustPos(departments, dept1, "left");
+                if checkSpace(departments, dept_number, "left")
+                    departments(dept1) = departments(dept1).move("left");
+                end
+                %}                
                 if departments(dept1).directions(1) == "up"
                     mod(2) = mod(2) - 1;
                 else
                     mod(1) = mod(1) + 1;
                 end
-            elseif departments(dept1).centroidX < departments(dept2).centroidX        
+            elseif departments(dept1).centroidX < departments(dept2).centroidX
+                %{
+                departments = adjustPos(departments, dept1, "right");
+                if checkSpace(departments, dept_number, "right")
+                    departments(dept1) = departments(dept1).move("right");
+                end
+                %}
                 if departments(dept1).directions(1) == "up"
                     mod(2) = mod(2) + 1;
                 else
@@ -26,12 +38,24 @@ function [ mod ] = attract(dept_number, departments, constraints)
             end
 
             if departments(dept1).centroidY > departments(dept2).centroidY
+                %{
+                departments = adjustPos(departments, dept1, "up");
+                if checkSpace(departments, dept_number, "up")
+                    departments(dept1) = departments(dept1).move("up");
+                end
+                %}
                 if departments(dept1).directions(1) == "up"
                     mod(1) = mod(1) + 1;
                 else
                     mod(2) = mod(2) + 1;
                 end
             elseif departments(dept1).centroidY < departments(dept2).centroidY
+                %{
+                departments = adjustPos(departments, dept1, "down");
+                if checkSpace(departments, dept_number, "down")
+                    departments(dept1) = departments(dept1).move("down");
+                end
+                %}
                 if departments(dept1).directions(1) == "up"
                     mod(1) = mod(1) - 1;
                 else
