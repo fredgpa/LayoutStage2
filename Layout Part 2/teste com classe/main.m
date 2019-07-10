df2209780d91c096ed9a0d7ae71ab1b7a544080e
fredgpa <fred_gpa@hotmail.com>
2019-07-10 10:47:08 -0300

    testando

--- Layout Part 2/teste com classe/main.m
+++ Layout Part 2/teste com classe/main.m
@@ -28,11 +28,10 @@ function [ result ] = main(departments, constraints, materials, costs)
 
         departments(dept_number) = departments(dept_number).updateDir();
     
-        %{
+        
         if ~isempty(constraints) && bool
             mod = attract(dept_number, departments, constraints);
         end
-        %}
 
         prob = dirProb(departments, dept_number, mod, bool);
 
